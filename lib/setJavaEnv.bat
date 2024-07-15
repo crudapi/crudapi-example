a85b9c73d428fb3daadf51d45f512ccf325d3e89
crudapi <admin@crudapi.cn>
2024-07-15 18:52:15 +0800

    jdk1.8.0_202

--- lib/setJavaEnv.bat
+++ lib/setJavaEnv.bat
@@ -1,7 +1,7 @@
 echo %Path%
 
-SETX /M JAVA_HOME "C:\Program Files\Java\jdk1.8.0_321"
+SETX /M JAVA_HOME "C:\Program Files\Java\jdk1.8.0_202"
 
-SETX /M Path "C:\Program Files\Java\jdk1.8.0_321\bin;%Path%"
+SETX /M Path "C:\Program Files\Java\jdk1.8.0_202\bin;%Path%"
 
 pause
